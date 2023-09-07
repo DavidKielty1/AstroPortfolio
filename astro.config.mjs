8ae468fb8f4866f18b545851de745c09806281e3
David Kielty <davidkielty@live.co.uk>
2023-09-07 17:58:49 +0100

    Upgraded to Astro 3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({ configFile: "./tailwind.config.cjs" }),
-    react(),
-    image(),
-  ],
+  integrations: [tailwind({ configFile: "./tailwind.config.cjs" }), react()],
 });
